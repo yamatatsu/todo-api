2fc8bd0dddb79a1cf01a57e8eff3f742842f7927
yamatatsu <the.kinnikuman@gmail.com>
2021-06-19 22:57:02 +0900

    refacter: add comment

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { getDbUrl } from "./constants";
 
 /**
+ * PrismaClient を生成する。
+ * AWS SecretManager から取得した情報をもとにDB接続するため、Prisma公式にあるような環境変数にDATABASE_URLを用意して接続する方法が使えない。
+ * そのため、`new PrismaClient()` の引数にurlを渡す方法で実装している。
+ *
  * node のファイル読み込みの時点でdbとのつなぎこみを開始するために、ファイルのトップレベルでPromiseを生成する
  */
 
