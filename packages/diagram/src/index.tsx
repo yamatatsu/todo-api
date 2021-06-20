4b3dcf144be3272649d086d43fdfbabd6da900c7
yamatatsu <the.kinnikuman@gmail.com>
2021-06-20 10:15:46 +0900

    diagram: init

---
+++ packages/diagram/src/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { PNG, Diagram, GeneralIcon } from "rediagram";
+import {
+  AWS,
+  InvizAWS,
+  EC2,
+  Lambda,
+  VPC,
+  RDS,
+  APIGateway,
+  SecretsManager,
+  Cognito,
+} from "@rediagram/aws";
+
+PNG(
+  <Diagram title="TODO API">
+    <InvizAWS>
+      <AWS>
+        <VPC>
+          <VPC type="Private subnet">
+            <Lambda
+              name="Express Server"
+              upstream={["MySQL", "Secrets for RDS"]}
+            />
+            <Lambda name="Migrater" upstream={["MySQL", "Secrets for RDS"]} />
+          </VPC>
+          <VPC type="Private subnet">
+            <RDS name="MySQL" type="Aurora" />
+          </VPC>
+        </VPC>
+        <SecretsManager name="Secrets for RDS" />
+        <APIGateway name="REST API" upstream={["Express Server"]} />
+        <Cognito name="UserPool" />
+      </AWS>
+      <GeneralIcon
+        name="Client"
+        type="Client"
+        upstream={["REST API", "UserPool"]}
+      />
+    </InvizAWS>
+  </Diagram>,
+  { dir: "dist" }
+);
