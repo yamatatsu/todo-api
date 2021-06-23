42cbfaef68e163f07f8413fe6975c1bbd0d16680
yamatatsu <the.kinnikuman@gmail.com>
2021-06-23 10:58:18 +0900

    シミュレータの機能をシンプルにした

--- packages/simulator/src/App.tsx
+++ packages/simulator/src/App.tsx
@@ -1,47 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Auth } from "aws-amplify";
 import "./App.css";
 
-const apiUrlBase = process.env.REACT_APP_API_URL_BASE;
-
 function App() {
-  const [name, setName] = React.useState<string>("");
+  const jwtToken = useJwtToken();
+
+  if (!jwtToken) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div className="App">
-      <div>
-        <h2>create user</h2>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
-        <button
-          onClick={() => {
-            Auth.currentSession().then((session) => {
-              const token = session.getIdToken().getJwtToken();
-              navigator.clipboard.writeText(
-                `curl -H "Content-Type: application/json" -H "x-ta-token:${token}" -X POST -d '{"name":"${name}"}' "${apiUrlBase}/user"`
-              );
-            });
-          }}
-        >
-          copy
-        </button>
-      </div>
-      <div>
-        <h2>get user</h2>
-        <button
-          onClick={() => {
-            Auth.currentSession().then((session) => {
-              const token = session.getIdToken().getJwtToken();
-              navigator.clipboard.writeText(
-                `curl -H "Content-Type: application/json" -H "x-ta-token:${token}" "${apiUrlBase}/user"`
-              );
-            });
-          }}
-        >
-          copy
-        </button>
-      </div>
+      <h1>JWT Token</h1>
+      <button
+        onClick={() => {
+          navigator.clipboard.writeText(jwtToken);
+        }}
+      >
+        copy jwtToken
+      </button>
     </div>
   );
 }
 
 export default withAuthenticator(App);
+
+function useJwtToken() {
+  const [token, setToken] = useState<string | null>(null);
+  useEffect(() => {
+    Auth.currentSession().then((session) => {
+      const token = session.getIdToken().getJwtToken();
+      setToken(token);
+    });
+  }, []);
+  return token;
+}
--- packages/simulator/src/awsComfigure.ts
+++ packages/simulator/src/awsComfigure.ts
@@ -2,5 +2,5 @@ export const authConfigure = {
   region: process.env.REACT_APP_REGION,
   userPoolId: process.env.REACT_APP_USER_POOL_ID,
   userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
-  authenticationFlowType: process.env.REACT_APP_AUTHENTICATION_FLOW_TYPE,
+  authenticationFlowType: "USER_PASSWORD_AUTH",
 };
